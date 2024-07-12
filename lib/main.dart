1ce814a71aa95c2ad9386144ab70aa97fbb8e137
SilverShadow123 <hasanmohasin852@gmail.com>
2024-07-12 23:32:40 +0600

    Updating

--- lib/main.dart
+++ lib/main.dart
@@ -41,21 +41,22 @@ class _HomeState extends State<Home> {
         title: Text('Test 03'),
         backgroundColor: Colors.blue,
       ),
-      body: ListView.separated(
-        itemCount: friendList.length,
-        itemBuilder: (context, index) {
-          return Card(
-           child: Image.asset(friendList[index]),
-          );
-        },
-        separatorBuilder: (context, index) {
-          return Divider(
-            height: 25,
-            color: Colors.blueAccent,
-          );
-        },
-
-      ),
+      // body:
+      // ListView.separated(
+      //   itemCount: friendList.length,
+      //   itemBuilder: (context, index) {
+      //     return Card(
+      //      child: Image.asset(friendList[index]),
+      //     );
+      //   },
+      //   separatorBuilder: (context, index) {
+      //     return Divider(
+      //       height: 25,
+      //       color: Colors.blueAccent,
+      //     );
+      //   },
+      //
+      // ),
 
       // body: GridView.builder(
       //     gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
@@ -65,16 +66,66 @@ class _HomeState extends State<Home> {
       //     ),
       //     itemCount: friendList.length,
       //     itemBuilder: (context, index) {
-      //       return Column(
-      //         children: [
-      //           Text(index.toString()),
-      //           Text(index.toString()),
-      //           Text(friendList[index]),
-      //         ],
+      //       return Card(
+      //         child: Image.asset(friendList[index]),
       //       );
       //     }
       //     ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                width: 100,
+                height: 100,
+                //color: Colors.green,
+                // padding: EdgeInsets.all(3),
+                margin: EdgeInsets.all(30),
+                alignment: Alignment.bottomRight,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.yellow, width: 5),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(16),
+                    bottomRight: Radius.circular(16),
+                  ),
+                  image: DecorationImage(
+                      image: AssetImage('assets/images/a-girl.jpg'),
+                      fit: BoxFit.cover),
+                  boxShadow: <BoxShadow>[
+                    BoxShadow(
+                      color: Colors.red.withOpacity(0.5),
+                      spreadRadius: 4,
+                      blurRadius: 4,
+                      offset: Offset(0, 15)
+                    )
+                  ]
+                ),
+                child: Text('Dragon'),
+              ),
+            ),
 
+            ElevatedButton(onPressed: (){
+              // showAboutDialog(context: context,applicationName: 'Sample', c;hildren: [Text('data')]);
+              showAboutDialog(context: context, );
+            }, child: Text('Dragon')),
+
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.green,
+              padding: EdgeInsets.all(30),
+              margin: EdgeInsets.all(30),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text('Heavenly Demon'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
