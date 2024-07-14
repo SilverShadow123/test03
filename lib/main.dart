f8210efdda31b9dba57ae5d9476d6c9a34183b0a
SilverShadow123 <hasanmohasin852@gmail.com>
2024-07-15 01:18:18 +0600

    Updating

--- lib/main.dart
+++ lib/main.dart
@@ -13,111 +13,42 @@ class test03 extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Home(),
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(color: Colors.deepPurpleAccent)
+      ),
     );
   }
 }
 
-class Home extends StatefulWidget {
+class Home extends StatelessWidget {
   const Home({super.key});
-
-  @override
-  State<Home> createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  List<String> friendList = [
-    'assets/images/a-girl.jpg',
-    'assets/images/above-the-mountains.png',
-    'assets/images/anime-brothers.jpg',
-    'assets/images/anime-coffee-girl.jpg',
-    'assets/images/another-anime-girl.jpg',
-    'assets/images/arch-girl.png'
-  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Test 03'),
-        backgroundColor: Colors.blue,
+        title: Center(child: Text('Test 05')),
+
       ),
-      body: Center(
+
+      body: Padding(
+        padding: const EdgeInsets.all(18.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                width: 100,
-                height: 100,
-                //color: Colors.green,
-                // padding: EdgeInsets.all(3),
-                margin: EdgeInsets.all(30),
-                alignment: Alignment.bottomRight,
-                decoration: BoxDecoration(
-                    border: Border.all(color: Colors.yellow, width: 5),
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(16),
-                      bottomRight: Radius.circular(16),
-                    ),
-                    image: DecorationImage(
-                        image: AssetImage('assets/images/a-girl.jpg'),
-                        fit: BoxFit.cover),
-                    boxShadow: <BoxShadow>[
-                      BoxShadow(
-                          color: Colors.red.withOpacity(0.5),
-                          spreadRadius: 4,
-                          blurRadius: 4,
-                          offset: Offset(0, 15))
-                    ]),
-                child: Text('Dragon'),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Enter Your Race',
+                hintStyle: TextStyle(
+                  color: Colors.blueGrey,
+                ),
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.blueGrey
+                  )
+                ),
               ),
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  // showAboutDialog(context: context,applicationName: 'Sample', children: [Text('data')]);
-                  showDialog(
-                      context: context,
-                      builder: (context) {
-                        return AlertDialog(
-                          title: Text('dungeon'),
-                          content: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              Text('asdf'),
-                              Text('asdf'),
-                              Text('asdf'),
-                              Text('asdf'),
-                            ],
-                          ),
-                          actions: [
-                            TextButton(
-                                onPressed: () {
-                                  Navigator.pop(context);
-                                },
-                                child: Text('Cancel')),
-                            TextButton(
-                                onPressed: () {
-                                  Navigator.pop(context);
-                                },
-                                child: Text('OK')),
-                          ],
-                        );
-                      });
-                },
-                child: Text('Dragon')),
-            Container(
-              width: 100,
-              height: 100,
-              color: Colors.green,
-              padding: EdgeInsets.all(30),
-              margin: EdgeInsets.all(30),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text('Heavenly Demon'),
-            ),
+
+            )
           ],
         ),
       ),
