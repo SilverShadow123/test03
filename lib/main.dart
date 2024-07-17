b6e703129793f624f99bd51e47a056de2afc60c9
SilverShadow123 <hasanmohasin852@gmail.com>
2024-07-17 10:18:08 +0600

    Updating

--- lib/main.dart
+++ lib/main.dart
@@ -13,24 +13,31 @@ class test03 extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Home(),
-      theme: ThemeData(
-        appBarTheme: AppBarTheme(color: Colors.deepPurpleAccent)
+      theme: ThemeData(appBarTheme: AppBarTheme(backgroundColor: Colors.blue)),
+      themeMode: ThemeMode.dark,
+      darkTheme: ThemeData(
+        brightness: Brightness.dark
       ),
     );
   }
 }
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  TextEditingController _descriptionComtroller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Center(child: Text('Test 05')),
-
       ),
-
       body: Padding(
         padding: const EdgeInsets.all(18.0),
         child: Column(
@@ -41,14 +48,66 @@ class Home extends StatelessWidget {
                 hintStyle: TextStyle(
                   color: Colors.blueGrey,
                 ),
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.blueGrey
-                  )
+                icon: Icon(Icons.add),
+                prefixIcon: Icon(Icons.search),
+                suffixIcon: TextButton(
+                  child: Text('Search'),
+                  onPressed: () {},
                 ),
+                label: Text('Name'),
+                focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.red)),
               ),
+            ),
+            SizedBox(
+              height: 4,
+            ),
 
-            )
+            //Description
+
+            TextField(
+              controller: _descriptionComtroller,
+              maxLines: 5,
+              maxLength: 200,
+              // onTap: (){
+              //   print('Tap on Textfield');
+              // },
+              textAlignVertical: TextAlignVertical.top,
+              decoration: InputDecoration(
+                hintText: 'Enter Description',
+                hintStyle: TextStyle(
+                  color: Colors.blueGrey,
+                ),
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.red),
+                ),
+                disabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.red)),
+                label: Text(
+                  'Description',
+                ),
+                focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.red)),
+              ),
+            ),
+            TextButton(
+                onPressed: () {
+                  _descriptionComtroller.clear();
+                },
+                child: Text('Clear')),
+            TextField(
+              obscureText: true,
+              controller: TextEditingController(),
+              decoration: InputDecoration(
+                hintText: 'Enter Your Password',
+                hintStyle: TextStyle(
+                  color: Colors.blueGrey,
+                ),
+                label: Text('Password'),
+                focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.red)),
+              ),
+            ),
           ],
         ),
       ),
